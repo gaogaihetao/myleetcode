4aca307607c47b584613c9b139be631d020a60a3
gaogaihetao <gaogaihetao@qq.com>
2019-07-11 10:43:58 +0800

    268. Missing Number

---
+++ 268.MissingNumber/MissingNumber.cpp
@@ -0,0 +1,9 @@
+int missingNumber(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++) {
+            sum+=i;
+            sum-=nums[i];
+        }
+        sum+=nums.size();
+        return sum;
+    }
